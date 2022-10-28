64a657513067441f6d40b9a13c19e773e47294aa
Lukasz Bierowiec <lbierowiec@g2a.com>
2022-10-28 10:26:17 +0200

    fix: storybook mock next/image

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,6 +4,13 @@ import commonEN from "../locales/en/common.json";
 
 import { ChakraProvider } from "@chakra-ui/react";
 
+import * as nextImage from "next/image";
+
+Object.defineProperty(nextImage, "default", {
+    configurable: true,
+    value: (props) => <img style={{position: 'absolute', inset: 0, objectFit: 'cover', height: '100%', width: "100%"}} {...props} />,
+});
+
 addDecorator((story) => {
     return (
         <I18nProvider
